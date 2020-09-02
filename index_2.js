4f8d1343e854c9cc95fe9aa8a813998bede87b54
zhukouskayat <64001084+zhukouskayat@users.noreply.github.com>
2020-09-02 16:24:27 +0300

    Update Matrix

--- index_2.js
+++ index_2.js
@@ -2,9 +2,8 @@
 
 // 2.1 Вывести матрицу таблицы умножения.
 
-console.log(`Задание 2.1`);
-
 const Matrix = [];
+console.log(`Матрица таблицы умножения`);
 
 for (let i = 1; i <= 9; i += 1) {
     const row = [];
@@ -24,8 +23,6 @@ for (let i = 0; i < 9; i += 1) {
 
 // 2.2 Посчитать сумму чисел находящихся на главной диагонали.
 
-console.log(`Задание 2.2`);
-
 let SumMainDiag = 0;
 
 for (let i = 0; i < 9; i += 1) {
@@ -35,8 +32,6 @@ for (let i = 0; i < 9; i += 1) {
 console.log(`Сумма чисел находящихся на главной диагонали = ${SumMainDiag}`);
 
 // 2.3 Посчитать среднее геометрическое чисел находящихся на вспомогательной диагонали.
-
-console.log(`Задание 2.3`);
 
 let Product = 1;
 let Count = 0;
@@ -50,42 +45,63 @@ const gAverage = Product / Count;
 
 console.log(`Среднее геометрическое чисел находящихся на вспомогательной диагонали = ${gAverage}`);
 
-// 2.4 Посчитать сумму всех элементов для каждого чётного столбца.
+// 2.4 Посчитать сумму всех всех чисел четных столбцов.
 
-console.log(`Задание 2.4`);
-
-let SumCol = 0;
+let SumAllCol = 0;
 
 for (let i = 1; i < 9; i += 2) {
     for (let j = 0; j < 9; j += 1) {
-        SumCol += Matrix[i][j];
+        SumAllCol += Matrix[i][j];
     }
 }
 
-console.log(`Сумма всех чисел четных столбцов = ${SumCol}`);
+console.log(`Сумма всех чисел четных столбцов = ${SumAllCol}`);
 
-// 2.5 Посчитать среднее арифметическое сумм нечетных строк.
+// 2.5 Посчитать сумму всех элементов для каждого чётного столбца.
 
-console.log(`Задание 2.5`);
+const SumCol = [];
+for (let i = 1; i < Matrix.length; i += 2) {
+    let sum = 0;
+    for (let j = 0; j < Matrix.length; j += 1) {
+        sum += Matrix[j][i];
+    }
+    SumCol.push(sum);
+}
+console.log(`Cумма всех элементов для каждого чётного столбца = ${SumCol}`);
 
-let SumRow = 0;
-let CountRow = 0;
+// 2.6 Посчитать среднее арифметическое сумм нечетных строк.
+
+let SumAllRow = 0;
+let CountAllRow = 0;
 
 for (let i = 0; i < 9; i += 1) {
     for (let j = 0; j < 9; j += 2) {
-        SumRow += Matrix[i][j];
-        CountRow += 1;
+        SumAllRow += Matrix[i][j];
+        CountAllRow += 1;
     }
 }
 
-const RowAverage = SumRow / CountRow;
+const RowAllAverage = SumAllRow / CountAllRow;
+
+console.log(`Среднее арифметическое сумм чисел нечетных строк = ${RowAllAverage}`);
+
+// 2.7 Посчитать среднее арифметическое сумм каждой нечётной строки
+
+const RowAverage = [];
+
+for (let i = 0; i < Matrix.length; i += 2) {
+    let SumRow = 0;
+    let CountRow = 0;
+    for (let j = 0; j < Matrix.length; j += 1) {
+        SumRow += Matrix[i][j];
+        CountRow += 1;
+    }
+    RowAverage.push(SumRow / CountRow);
+}
 
 console.log(`Среднее арифметическое сумм чисел нечетных строк = ${RowAverage}`);
 
-
-// 2.6 Посчитать сумму всех чисел матрицы
-
-console.log(`Задание 2.6`);
+// 2.8 Посчитать сумму всех чисел матрицы
 
 let SumMatrix = 0;
 
@@ -97,19 +113,7 @@ for (let i = 0; i < 9; i += 1) {
 
 console.log(`Сумма всех чисел матрицы = ${SumMatrix}`);
 
-// 2.7 Посчитать сумму чисел в верхнем треугольнике относительно главной диагонали не включая и включая саму диагональ
-
-console.log(`Задание 2.7`);
-
-const SumUpperTr = (SumMatrix - SumMainDiag) / 2;
-const SumUpperTrDiag = SumUpperTr + SumMainDiag;
-
-console.log(`Сумма чисел в верхнем треугольнике относительно главной диагонали (без главной диагонали) = ${SumUpperTr}`);
-console.log(`Сумма чисел в верхнем треугольнике относительно главной диагонали (c главной диагональю) = ${SumUpperTrDiag}`);
-
-// 2.8 Посчитать сумму чисел находящихся на вспомогательной диагонали
-
-console.log(`Задание 2.8`);
+// 2.9 Посчитать сумму чисел находящихся на вспомогательной диагонали
 
 let SumSecondDiag = 0;
 
@@ -119,36 +123,62 @@ for (let i = 0; i < 9; i += 1) {
 
 console.log(`Сумма чисел находящихся на вспомогательной диагонали = ${SumSecondDiag}`);
 
-// 2.9 Посчитать сумму чисел в нижнем треугольнике относительно вспомогательной диагонали
+// 2.10 Найти максимальное и минимальное число для всей матрицы и для каждой строки
 
-console.log(`Задание 2.9`);
+let MaxMatrix = Matrix[0][0];
+let MinMatrix = Matrix[0][0];
 
-const SumDownTr = (SumMatrix - SumSecondDiag) / 2;
-const SumDownTrDiag = SumDownTr + SumSecondDiag;
+for (let i = 0; i < Matrix.length; i += 1) {
+    for (let j = 0; j < Matrix.length; j += 1) {
+        if (MaxMatrix < Matrix[i][j]) {
+            MaxMatrix = Matrix[i][j];
+        }
+        if (MinMatrix > Matrix[i][j]) {
+            MinMatrix = Matrix[i][j];
+        }
+    }
+}
 
-console.log(`Сумма чисел в нижнем треугольнике относительно вспомогательной диагонали (без главной диагонали) = ${SumDownTr}`);
-console.log(`Сумма чисел в нижнем треугольнике относительно вспомогательной диагонали (c главной диагональю) = ${SumDownTrDiag}`);
-
-// // 2.10 Найти максимальное и минимальное число для каждой строки.
-
-console.log(`Задание 2.10`);
+console.log(`Максимальное число для всей матрицы = ${MaxMatrix}`);
+console.log(`Минимальное число для всей матрицы = ${MinMatrix}`);
 
 let MaxRow = Matrix[0];
+let MinRow = Matrix[0];
 
-for (let i = 1; i < Matrix.length; i += 1) {
+for (let i = 0; i < Matrix.length; i += 1) {
     if (MaxRow < Matrix[i]) {
         MaxRow = Matrix[i];
     }
 }
 
-console.log(`Максимальные числа для каждой строки = ${MaxRow}`);
-
-let MinRow = Matrix[0];
-
-for (let i = 1; i < Matrix.length; i += 1) {
+for (let i = 0; i < Matrix.length; i += 1) {
     if (MinRow > Matrix[i]) {
         MinRow = Matrix[i];
     }
 }
 
+console.log(`Максимальные числа для каждой строки = ${MaxRow}`);
 console.log(`Минимальные числа для каждой строки = ${MinRow}`);
+
+// 2.11 Посчитать сумму чисел в верхнем треугольнике относительно главной диагонали
+
+let SumUpperTriangle = 0;
+
+for (let i = 0; i < 9; i += 1) {
+    for (let j = i; j < 9; j += 1) {
+        SumUpperTriangle += Matrix[j][i];
+    }
+}
+console.log(`Сумма чисел в верхнем треугольнике относительно главной диагонали = ${SumUpperTriangle}`);
+
+// 2.12 Посчитать сумму чисел в верхнем треугольнике относительно вспомогательной диагонали
+
+let SumUpTriangle = 0;
+
+for (let i = 0; i < 9; i += 1) {
+    for (let j = 0; j < 9 - i; j += 1) {
+        SumUpTriangle += Matrix[j][i];
+    }
+}
+
+console.log(`Сумма чисел в верхнем треугольнике относительно вспомогательной диагонали = ${SumUpTriangle}`);
